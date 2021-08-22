27edc7b729fccf88ed14fa7b52b49ed991362aa0
Krishna Prasad Bharathula <krishnaprasad.bharathula@gmail.com>
2021-08-22 15:06:13 +0200

    Added code for first two problems

---
+++ usingfunctions.R
@@ -0,0 +1,13 @@
+#
+# take input from the user
+{
+  num = as.integer(readline(prompt = "Enter a number: "))
+  if (num < 0) {
+    print("Enter a positive number")
+  } else {
+    sum = (num * (num + 1)) / 2
+    
+    print(paste("The sum is", sum))
+  }
+}
+
